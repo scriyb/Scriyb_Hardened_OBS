3f72c153d9184451b109c3811556b7d87f993d44
Robert Ota Dieterich <rob@skyboygames.com>
2018-03-01 12:21:15 -0500

    Volume control checkboxes now show keyboard focus

--- UI/mute-checkbox.hpp
+++ UI/mute-checkbox.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
 #include <QCheckBox>
+#include "visibility-checkbox.hpp"
 
-class MuteCheckBox : public QCheckBox {
+class MuteCheckBox : public VisibilityCheckBox_Base {
 	Q_OBJECT
+
+public:
+	MuteCheckBox() :
+		VisibilityCheckBox_Base(
+			":/res/images/mute.png",
+			":/res/images/unmute.png",
+			":/res/images/mute_focused.png",
+			":/res/images/unmute_focused.png"
+		)
+	{
+	}
 };
